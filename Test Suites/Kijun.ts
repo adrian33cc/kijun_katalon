21aeb7fcf5018007de9ec710da6e4971a0ceb8d2
adrian33cc <adrian.33ice@hotmail.com>
2022-11-09 14:44:42 -0600

    Add test suite

---
+++ Test Suites/Kijun.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Kijun</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c7e3e0c5-4c8c-4432-82fa-885689b14289</testSuiteGuid>
+</TestSuiteEntity>
